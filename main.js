4c90f0e2025631da42641a6bece7bf7ed7cc6ce1
qindewen <1037055363@qq.com>
2019-12-19 10:12:58 +0800

    Finish colour change for header buttons

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
-for (let header in document.getElementsByClassName("header")) {
-    header.addEventListener('click', () => {
-        document.querySelector('.active').classList.remove('active')
-        header.classList.add('active')
+const headers = document.getElementsByClassName("background")
+for (let header = 0; header < headers.length; header++) {
+    headers[header].addEventListener('click', () => {
+        // 把点击项变成蓝色
+        headers[header].style = "background-color: #344BFF;"
+        // 把其他几项换回灰色
+        for (let i = 0; i < headers.length; i++) {
+            if (header != i) {
+                headers[i].style = "background-color: #cccccc;"
+            }
+        }
     })
 }
