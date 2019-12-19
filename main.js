93b1a2a727f2349f9fa81984226d04bcd4f3318b
qindewen <1037055363@qq.com>
2019-12-19 16:58:40 +0800

    Clean up wepage model

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
-const images = document.getElementsByClassName("image")
-for (let image = 0; image < images.length; image++) {
-    images[image].addEventListener('click', () => {
+const icons = document.getElementsByClassName("icon")
+for (let icon = 0; icon < icons.length; icon++) {
+    icons[icon].addEventListener('click', () => {
         // 如果点击项不是蓝色就把它变成蓝色
-        if (images[image].src.indexOf('active') === -1) {
-            images[image].src = images[image].src.split('.')[0] + '_active.' + images[image].src.split('.')[1]
+        if (icons[icon].src.indexOf('active') === -1) {
+            icons[icon].src = icons[icon].src.split('.')[0] + '_active.' + icons[icon].src.split('.')[1]
         }
 
         // 把其他几项换回灰色
-        for (let i = 0; i < images.length; i++) {
-            if (image != i) {
-                if (images[i].src.indexOf('active') !== -1) {
-                    //console.log(images[i].src.slice(0, -11))
-                    images[i].src = images[i].src.slice(0, -11) + '.png'
+        for (let i = 0; i < icons.length; i++) {
+            if (icon != i) {
+                if (icons[i].src.indexOf('active') !== -1) {
+                    //console.log(icons[i].src.slice(0, -11))
+                    icons[i].src = icons[i].src.slice(0, -11) + '.png'
                 }
             }
         }
