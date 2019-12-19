13be236dc8d4215c0003f8ead1dbbfacadbb2f98
qindewen <1037055363@qq.com>
2019-12-19 17:51:51 +0800

    Vue basic structure

--- main.js
+++ main.js
@@ -4,14 +4,13 @@ for (let icon = 0; icon < icons.length; icon++) {
         // 如果点击项不是蓝色就把它变成蓝色
         if (icons[icon].src.indexOf('active') === -1) {
             icons[icon].src = icons[icon].src.split('.')[0] + '_active.' + icons[icon].src.split('.')[1]
-        }
 
-        // 把其他几项换回灰色
-        for (let i = 0; i < icons.length; i++) {
-            if (icon != i) {
-                if (icons[i].src.indexOf('active') !== -1) {
-                    //console.log(icons[i].src.slice(0, -11))
-                    icons[i].src = icons[i].src.slice(0, -11) + '.png'
+            // 把其他几项换回灰色
+            for (let i = 0; i < icons.length; i++) {
+                if (icon != i) {
+                    if (icons[i].src.indexOf('active') !== -1) {
+                        icons[i].src = icons[i].src.slice(0, -11) + '.png'
+                    }
                 }
             }
         }
