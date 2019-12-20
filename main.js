2dc68daa599536cda8bd28f24c321a7292bd9db9
qindewen <1037055363@qq.com>
2019-12-20 18:00:25 +0800

    Finish homepage

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
+const API_URL = 'http://v.sogx.cn'
 const icons = document.getElementsByClassName("icon")
 for (let icon = 0; icon < icons.length; icon++) {
     icons[icon].addEventListener('click', () => {
-        // 如果点击项不是蓝色就把它变成蓝色
+        // 如果点击项不是蓝色
         if (icons[icon].src.indexOf('active') === -1) {
             icons[icon].src = icons[icon].src.split('.')[0] + '_active.' + icons[icon].src.split('.')[1]
 
@@ -13,6 +14,32 @@ for (let icon = 0; icon < icons.length; icon++) {
                     }
                 }
             }
+
+            const root = document.getElementById('root')
+            // 清除当前列表
+            root.removeChild(document.getElementById('ul'))
         }
     })
 }
+
+new Vue({
+    el: '#root',
+    mounted() {
+        const root = document.getElementById('root')
+        const ul = document.createElement('ul')
+        root.appendChild(ul)
+        ul.setAttribute('id', 'ul')
+        axios.get(API_URL + "/api/zwfw/classify_list/ym_id/48/type/1").then(response => {
+            for (icon in response.data.data) {
+                const li = document.createElement('li')
+                ul.appendChild(li)
+                const img = document.createElement('img')
+                img.setAttribute('src', API_URL + response.data.data[icon].imgurl)
+                const p = document.createElement('p')
+                p.innerText = response.data.data[icon].name
+                li.appendChild(img)
+                li.appendChild(p)
+            }
+        })
+    }
+})
