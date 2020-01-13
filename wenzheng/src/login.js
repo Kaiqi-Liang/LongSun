b28336dea670f6953d12b1bee96c79d0a868add9
梁凯祺 <137055338@qq.com>
2020-01-13 17:33:07 +0800

    Add my own login for master branch

---
+++ wenzheng/src/login.js
@@ -0,0 +1,25 @@
+const API_URL = 'http://test.sogx.cn'
+const id = location.href.split('?')[1].split('=')[1]
+document.querySelector('.login').addEventListener('click', (event) => {
+    event.preventDefault()
+    const input = document.getElementsByTagName('input')
+    if (!input[0].value) {
+        layer.msg('请输入用户名', {offset: 'b'})
+    } else if (!input[1].value) {
+        layer.msg('请输入密码', {offset: 'b'})
+    } else {
+        const modal = document.querySelector('.modal')
+        modal.style.display = 'block'
+ 
+        const data = new FormData(document.getElementById('form'))
+        data.append('appid', id)
+        axios.post(API_URL + '/api/user/login', data)
+            .then(response => {
+                layer.msg(response.data.msg, { offset: 'b' })
+                setTimeout(() => {
+                    modal.style.display = 'none'
+                    if (response.data.msg == '登录成功') history.go(-1)
+                }, 500)
+            })
+    }
+})
