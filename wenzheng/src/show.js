dba49d66d6f162d99edc7e5a410c61cbdd081288
梁凯祺 <137055338@qq.com>
2020-01-17 11:00:07 +0800

    Change time

--- wenzheng/src/show.js
+++ wenzheng/src/show.js
@@ -161,30 +161,22 @@ new Vue({
             const date = new Date(time * 1000)
             const now = new Date()
 
-            const years = parseInt(now.getYear()) - parseInt(date.getYear());
-            const months = parseInt(now.getMonth()) - parseInt(date.getMonth());
             const days = parseInt(now.getDate()) - parseInt(date.getDate());
             const hours = parseInt(now.getHours()) - parseInt(date.getHours());
             const minutes = parseInt(now.getMinutes()) - parseInt(date.getMinutes());
             const seconds = parseInt(now.getSeconds()) - parseInt(date.getSeconds());
-            if (years === 1) {
-                time = '去年';
-            } else if (years === 0 && months > 1) {
-                time = months + '月前';
-            } else if (months === 1) {
-                time = '上个月';
-            } else if (months === 0 && days > 1) {
-                time = days + '天前';
-            } else if (days === 1) {
-                time = '昨天';
-            } else if (days === 0 && hours > 0) {
-                time = hours + '小时前';
-            } else if (hours === 0 && minutes > 0) {
-                time = minutes + '分钟前';
-            } else if (minutes === 0 && seconds > 0) {
-                time = seconds + '秒前';
+            if (days > 7) {
+                time = this.formatDate(time)
+            } else if (days > 1) {
+                time = days + '天前'
+            } else if (days == 1) {
+                time = '昨天'
+            } else if (hours > 0) {
+                time = hours + '小时前'
+            } else if (minutes > 0) {
+                time = minutes + '分钟前'
             } else {
-                time = years + '年前';
+                time = seconds + '秒前'
             }
             return time
         }
