51c7f2199b1571b189e41646e49392286ffa710d
梁凯祺 <137055338@qq.com>
2020-01-17 15:11:14 +0800

    Fix login path again

--- wenzheng/src/guestbook.js
+++ wenzheng/src/guestbook.js
@@ -204,7 +204,7 @@ new Vue({
                 .then(json => {
                     layer.msg(json.msg)
                     if (json.msg == '未登录') {
-                        setTimeout(() => aLinkClick('../../wap/my/login/appid/' + this.ym_id), 300)
+                        setTimeout(() => top.location.href = 'login.html?appid=' + this.ym_id, 300)
                     }
                 })
         }
